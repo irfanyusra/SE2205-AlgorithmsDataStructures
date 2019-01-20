7329d759000e08cbfc2ceeb3099a841f126f135e
yusra <yusrairfan99@hotmail.com>
2019-01-20 13:25:44 -0500

    Newton Square Root implemented

--- Lab1/src/com/company/Main.java
+++ Lab1/src/com/company/Main.java
@@ -1,8 +1,36 @@
 package com.company;
 
+/**
+ * Lab 1
+ * @author: irfanyusra
+ */
+
+import static java.lang.Math.abs;
+import java.util.Scanner;
+
 public class Main {
 
     public static void main(String[] args) {
-	// write your code here
+        Scanner reader = new Scanner(System.in);  // Reading from System.in
+        System.out.println("Recursion ");
+	    System.out.println("--Newton Square Root--");
+	    System.out.println("Enter a value to calculate the square root: ");
+        System.out.println("Enter a number: ");
+        double x = reader.nextDouble(); // Scans the next token of the input as an double.
+	    double a = x/2;
+	    double eps = 1E-6;
+	    double ans = newtonSqrt( x,  a,  eps);
+	    System.out.print(ans);
+
+    }
+
+    public static double newtonSqrt(double x, double a, double eps){
+       double y = abs (a*a-x);
+       if (y<eps)
+           return a;
+       else {
+           a = (a + x / a) / 2;
+           return newtonSqrt(x, a, eps);
+       }
     }
 }

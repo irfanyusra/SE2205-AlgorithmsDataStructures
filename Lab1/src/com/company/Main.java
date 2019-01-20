d6e1216b9d8f9c2ba7b8b619210e2ddaa64997e4
yusra <yusrairfan99@hotmail.com>
2019-01-20 16:28:34 -0500

    findMinChangeCoins method implemented

--- Lab1/src/com/company/Main.java
+++ Lab1/src/com/company/Main.java
@@ -9,20 +9,57 @@ import static java.lang.Math.abs;
 import java.util.Scanner;
 
 public class Main {
+    public static int S[]={1, 5, 10, 20, 25};
 
     public static void main(String[] args) {
         Scanner reader = new Scanner(System.in);  // Reading from System.in
         System.out.println("Recursion ");
 	    System.out.println("--Newton Square Root--");
 	    System.out.println("Enter a value to calculate the square root: ");
-        System.out.println("Enter a number: ");
         double x = reader.nextDouble(); // Scans the next token of the input as an double.
 	    double a = x/2;
 	    double eps = 1E-6;
 	    double ans = newtonSqrt( x,  a,  eps);
-	    System.out.print(ans);
+	    System.out.println(ans);
+
+        System.out.println("--Coin Change--");
+        System.out.println("Enter a change value: ");
+        int c = reader.nextInt();
+        int min = findMinChangeCoins (c, 0);
+        System.out.println(min);
 
     }
+//understand it better
+    public static int findMinChangeCoins(int C, int n){
+        if (C == 0)
+            return 1;
+        if (C < 0)
+            return 0;
+
+       /* int combo=0;
+        int minCombo = 0;
+        for (int coin = n; coin < S.length; coin++){
+            combo+= findMinChangeCoins (C - S[coin], coin);
+        }
+        return combo;*/
+        // Initialize result
+        int res=Integer.MAX_VALUE;
+
+        // Try every coin that has smaller value than C
+        for (int i=0; i<S.length ; i++)
+        {
+            if (S[i] <= C)
+            {
+                int temp = findMinChangeCoins( C- S[i], S.length);
+
+                // see if result can minimized
+                if (temp + 1 < res)
+                    res = temp + 1;
+            }
+        }
+        return res;
+    }
+
 
     public static double newtonSqrt(double x, double a, double eps){
        double y = abs (a*a-x);
@@ -33,4 +70,6 @@ public class Main {
            return newtonSqrt(x, a, eps);
        }
     }
+
+
 }
